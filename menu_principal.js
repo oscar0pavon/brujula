d2b23e57bacbfda331c7a7dbc8693d15d8d09976
Zaquimol <brunimolinas@gmail.com>
2026-01-31 07:13:23 -0300

    Menu Principal creado con fondo y boton

--- menu_principal.js
+++ menu_principal.js
@@ -6,13 +6,19 @@ class MenuPrincipal extends Phaser.Scene {
 
   preload() {
     // Cargamos el fondo y el logo
+    this.load.image('oceano', 'activos/oceano/oceano1.jpg')
     this.load.image('fondo_menu', 'activos/fondo_menu/fondo_menu.png');
+    this.load.image('logo', 'activos/logo-juego.png');
   }
 
   create() {
     // imagen de fondo 
+    this.fondoMenu = this.add.tileSprite(0, 0, 1280, 720, 'oceano').setOrigin(0, 0);
     let fondo = this.add.image(640, 360, 'fondo_menu');
     fondo.setDisplaySize(1280, 720);
+
+    //logo
+    let logo = this.add.image(640, 250, 'logo').setScale(0.4);
 
 
     // Botón de EMPEZAR
