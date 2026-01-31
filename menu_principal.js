30048f85c2ed26e07deb6ccb95d28c860fa6b3c4
Zaquimol <brunimolinas@gmail.com>
2026-01-31 06:57:13 -0300

    Menu Principal creado

--- menu_principal.js
+++ menu_principal.js
@@ -1,18 +1,46 @@
-
 class MenuPrincipal extends Phaser.Scene {
 
   constructor() {
-        super('MenuPrincipal');
+    super('MenuPrincipal');
   }
 
   preload() {
-
+    // Cargamos el fondo y el logo
+    this.load.image('fondo_menu', 'activos/fondo_menu/fondo_menu.png');
   }
 
   create() {
-    this.add.text(640, 360, 'EMPEZAR', { fill: '#fff' })
-            .setInteractive()
-            .on('pointerdown', () => this.scene.start('Brujula'));
-  }
+    // imagen de fondo 
+    let fondo = this.add.image(640, 360, 'fondo_menu');
+    fondo.setDisplaySize(1280, 720);
 
+
+    // Botón de EMPEZAR
+    let botonEmpezar = this.add.text(640, 450, ' EMPEZAR JUEGO ', {
+      fontSize: '40px',
+      fill: '#ffffff',
+      backgroundColor: '#0077be', // Color azul océano
+      padding: { x: 20, y: 10 },
+      fontStyle: 'bold'
+    })
+      .setOrigin(0.5)
+      .setInteractive({ useHandCursor: true }); // Cambia el cursor a una manito
+
+    //eventos del boton
+
+    // Inicia la escena de la Brújula
+    botonEmpezar.on('pointerdown', () => {
+      this.scene.start('Brujula');
+    });
+
+    // Efecto al pasar el mouse 
+    botonEmpezar.on('pointerover', () => {
+      botonEmpezar.setStyle({ fill: '#ff0', backgroundColor: '#005a8e' });
+    });
+
+    // Efecto al sacar el mouse
+    botonEmpezar.on('pointerout', () => {
+      botonEmpezar.setStyle({ fill: '#fff', backgroundColor: '#0077be' });
+    });
+  }
 }
