438671fb61dfeff99da6e2f4b668f7d52de96e1f
Zaquimol <brunimolinas@gmail.com>
2026-01-31 00:19:07 -0300

    Brujula parte 1

--- juego.js
+++ juego.js
@@ -7,6 +7,7 @@ class MainScene extends Phaser.Scene {
         //Cargar assets
         this.load.image('logo', 'activos/logo-juego.png');
         this.load.image('jugador', 'activos/barco/frente.png');
+        this.load.image('brujula', 'activos/brujula/brujula.png');
     }
 
 
@@ -19,6 +20,9 @@ class MainScene extends Phaser.Scene {
         let logo = this.add.image(40, 40, 'logo');
         logo.setScale(0.1);
 
+        this.brujula = this.add.image(1200, 600, 'brujula');
+        this.brujula.setScale(0.2);
+
         this.jugador = this.physics.add.sprite(640, 360, 'jugador');
         this.jugador.setScale(0.2);
         this.jugador.setCollideWorldBounds(true); // Para que no se salga de la pantalla
@@ -29,6 +33,9 @@ class MainScene extends Phaser.Scene {
             left: Phaser.Input.Keyboard.KeyCodes.A,
             right: Phaser.Input.Keyboard.KeyCodes.D
         });
+
+        //isla
+        this.isla = { x: 3600, y: 600 };
 
         // Agregar fisicas al jugador (sprite)
         // const jugador = this.physics.add.sprite(100, 100, 'jugador'); 
@@ -43,9 +50,16 @@ class MainScene extends Phaser.Scene {
             this.jugador.rotation = this.jugador.body.angle;
         }
 
-        // Resetear velocidad en cada frame para que no se mueva solo
-        this.jugador.setVelocity(0);
+        let anguloAlaIsla = Phaser.Math.Angle.Between(
+            this.jugador.x, this.jugador.y,
+            this.isla.x, this.isla.y
+        );
 
+        // Aplicamos la rotación a la brújula (sumamos 1.57 por que la imagen de brújula apunta originalmente a la derecha)
+        this.brujula.rotation = anguloAlaIsla + 1.57;
+
+
+        this.jugador.setVelocity(0);
         if (this.teclas.left.isDown) {
             this.jugador.setVelocityX(-velocidad);
         } else if (this.teclas.right.isDown) {
