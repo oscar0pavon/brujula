942a1205065624113e093585759f994030ecbb10
Oscar Pavon <pavon_oscar@outlook.com>
2026-01-31 00:17:28 -0300

    agreado oceano en movimiento

--- juego.js
+++ juego.js
@@ -5,8 +5,10 @@ class MainScene extends Phaser.Scene {
     preload() {
         //Cargar los activos aqui (imagenes, sonidos, etc)
         //Cargar assets
+        this.load.image('oceano', 'activos/oceano/oceano1.jpg')
         this.load.image('logo', 'activos/logo-juego.png');
         this.load.image('jugador', 'activos/barco/frente.png');
+      
     }
 
 
@@ -19,9 +21,16 @@ class MainScene extends Phaser.Scene {
         let logo = this.add.image(40, 40, 'logo');
         logo.setScale(0.1);
 
+        var dimension_oceano = 2000;
+        this.oceano = this.add.tileSprite(0,0, dimension_oceano,
+          dimension_oceano, "oceano").setOrigin(0,0);
+
+        this.cameras.main.setBounds(0, 0 , dimension_oceano, dimension_oceano);
+
+
         this.jugador = this.physics.add.sprite(640, 360, 'jugador');
         this.jugador.setScale(0.2);
-        this.jugador.setCollideWorldBounds(true); // Para que no se salga de la pantalla
+        //this.jugador.setCollideWorldBounds(true); // Para que no se salga de la pantalla
 
         this.teclas = this.input.keyboard.addKeys({
             up: Phaser.Input.Keyboard.KeyCodes.W,
@@ -29,6 +38,8 @@ class MainScene extends Phaser.Scene {
             left: Phaser.Input.Keyboard.KeyCodes.A,
             right: Phaser.Input.Keyboard.KeyCodes.D
         });
+        
+        this.cameras.main.startFollow(this.jugador, true, 0.1, 0.1);
 
         // Agregar fisicas al jugador (sprite)
         // const jugador = this.physics.add.sprite(100, 100, 'jugador'); 
@@ -36,6 +47,12 @@ class MainScene extends Phaser.Scene {
 
     //Funcion que se repite cada frame
     update() {
+
+        var velocidad_movimiento_olas = 0.06;
+
+        this.oceano.tilePositionX += velocidad_movimiento_olas;
+        this.oceano.tilePositionY += velocidad_movimiento_olas;
+
         // Velocidad constante
         const velocidad = 300;
 
